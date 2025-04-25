73deb5e3dc7f220d7a3c17a5ca3ab12064c845e1
koljapluemer <github@koljapluemer.com>
2025-04-25 17:14:54 +0200

    add basic vocab practice

--- withvideos/views/snippets/detail.py
+++ withvideos/views/snippets/detail.py
@@ -1,5 +1,7 @@
 from django.views.generic import DetailView
 from withvideos.models import Snippet
+import json
+from random import shuffle
 
 class SnippetDetailView(DetailView):
     model = Snippet
@@ -9,4 +11,20 @@ class SnippetDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['mode'] = self.kwargs.get('mode', 'practice')  # Default to practice mode
+        
+        # Prepare words data for Alpine.js
+        words_data = []
+        for word in self.object.words.all():
+            words_data.append({
+                'id': word.id,
+                'original_word': word.original_word,
+                'meanings': [meaning.en for meaning in word.meanings.all()]
+            })
+        
+        # Randomize the order
+        shuffle(words_data)
+        
+        # Add to context as JSON
+        context['words_json'] = json.dumps(words_data)
+        
         return context 
