a4dd7c2fb6976beff7cd824f3d1045d09501957f
koljapluemer <github@koljapluemer.com>
2025-04-30 18:48:19 +0200

    adapt static files to custom domain

--- samlearns/settings.py
+++ samlearns/settings.py
@@ -156,22 +156,22 @@ AWS_ACCESS_KEY_ID = env('R2_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('R2_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('R2_BUCKET_NAME')
 AWS_S3_ENDPOINT_URL = f"https://{env('R2_ACCOUNT_ID')}.r2.cloudflarestorage.com"
-AWS_S3_CUSTOM_DOMAIN = None
+AWS_S3_CUSTOM_DOMAIN = 'samlearns.com'
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_S3_FILE_OVERWRITE = False
 AWS_QUERYSTRING_AUTH = False
-AWS_S3_REGION_NAME = 'auto'  # Required for R2
-AWS_S3_ADDRESSING_STYLE = 'virtual'  # Required for R2
+AWS_S3_REGION_NAME = 'auto'
+AWS_S3_ADDRESSING_STYLE = 'virtual'
 
 # Static files configuration
 if DEBUG:
     STATIC_URL = '/static/'
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 else:
-    STATIC_URL = f'https://{env("R2_ACCOUNT_ID")}.r2.cloudflarestorage.com/{AWS_STORAGE_BUCKET_NAME}/'
+    STATIC_URL = 'https://samlearns.com/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
