407abc7e99a82efd7c5579c32693c21c084dbbf2
koljapluemer <github@koljapluemer.com>
2025-04-30 18:35:59 +0200

    improve R2 settings and urls for images

--- samlearns/settings.py
+++ samlearns/settings.py
@@ -156,23 +156,23 @@ AWS_ACCESS_KEY_ID = env('R2_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('R2_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('R2_BUCKET_NAME')
 AWS_S3_ENDPOINT_URL = f"https://{env('R2_ACCOUNT_ID')}.r2.cloudflarestorage.com"
-AWS_S3_CUSTOM_DOMAIN = f"{env('R2_BUCKET_NAME')}.r2.dev"
+AWS_S3_CUSTOM_DOMAIN = None
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+AWS_S3_FILE_OVERWRITE = False
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_REGION_NAME = 'auto'  # Required for R2
+AWS_S3_ADDRESSING_STYLE = 'virtual'  # Required for R2
 
 # Static files configuration
 if DEBUG:
     STATIC_URL = '/static/'
-    MEDIA_URL = '/media/'
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 else:
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+    STATIC_URL = f'https://{env("R2_ACCOUNT_ID")}.r2.cloudflarestorage.com/{AWS_STORAGE_BUCKET_NAME}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = []
