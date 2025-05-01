9bfffb9fdf6d69451399b8940618ee6a27a6286a
koljapluemer <github@koljapluemer.com>
2025-05-01 19:45:13 +0200

    split up templates

--- uke_fingerpicking/urls/app.py
+++ uke_fingerpicking/urls/app.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from uke_fingerpicking.views.app.tab_sheet_list import TabSheetListView
-from uke_fingerpicking.views.app.tab_sheet_practice import TabSheetPracticeView
+from uke_fingerpicking.views.app.tab_sheet_practice import (
+    TabSheetPracticeSetupView,
+    TabSheetPracticeActiveView,
+    TabSheetPracticeResultsView
+)
 
 app_name = 'uke_fingerpicking'
 
 urlpatterns = [
     path('', TabSheetListView.as_view(), name='tab_sheet_list'),
-    path('practice/<int:pk>/', TabSheetPracticeView.as_view(), name='tab_sheet_practice'),
+    path('practice/<int:pk>/', TabSheetPracticeSetupView.as_view(), name='tab_sheet_practice'),
+    path('practice/<int:pk>/active/', TabSheetPracticeActiveView.as_view(), name='tab_sheet_practice_active'),
+    path('practice/<int:pk>/results/', TabSheetPracticeResultsView.as_view(), name='tab_sheet_practice_results'),
 ]
--- uke_fingerpicking/views/app/tab_sheet_practice.py
+++ uke_fingerpicking/views/app/tab_sheet_practice.py
@@ -1,16 +1,46 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, TemplateView
 from uke_fingerpicking.models import TabSheet, Beat
 from django.utils.safestring import mark_safe
 import json
 
-class TabSheetPracticeView(DetailView):
+class TabSheetPracticeSetupView(DetailView):
     model = TabSheet
-    template_name = 'uke_fingerpicking/app/tab_sheet_practice.html'
+    template_name = 'uke_fingerpicking/app/tab_sheet_practice_setup.html'
     context_object_name = 'tab_sheet'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Get all beats ordered by index
         beats = list(Beat.objects.filter(tab_sheet=self.object).order_by('index').values('a', 'e', 'c', 'g'))
-        context['beats_json'] = mark_safe(json.dumps(beats))
+        context['beats_json'] = json.dumps(beats)
+        return context
+
+class TabSheetPracticeActiveView(DetailView):
+    model = TabSheet
+    template_name = 'uke_fingerpicking/app/tab_sheet_practice_active.html'
+    context_object_name = 'tab_sheet'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        beats = list(Beat.objects.filter(tab_sheet=self.object).order_by('index').values('a', 'e', 'c', 'g'))
+        context['beats_json'] = json.dumps(beats)
+        context['initial_bpm'] = self.request.GET.get('bpm', 40)
+        context['tab_sheet_data'] = {
+            'id': self.object.id,
+            'title': self.object.title,
+            'artist': self.object.artist
+        }
+        return context
+
+class TabSheetPracticeResultsView(DetailView):
+    model = TabSheet
+    template_name = 'uke_fingerpicking/app/tab_sheet_practice_results.html'
+    context_object_name = 'tab_sheet'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tab_sheet_data'] = {
+            'id': self.object.id,
+            'title': self.object.title,
+            'artist': self.object.artist
+        }
         return context 
