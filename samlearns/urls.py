8f800cc90144a114c1023fa5527f0b31853ee4ee
koljapluemer <github@koljapluemer.com>
2025-05-02 12:54:15 +0200

    minor setup

--- samlearns/urls.py
+++ samlearns/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cms/', include('cms.urls')),
     path('trees-of-germany/', include('trees_of_germany.urls')),
-    path('withvideos/', include('withvideos.urls')),
+    path('german-with-videos/', include('german_with_videos.urls')),
     path('uke-fingerpicking/', include('uke_fingerpicking.urls')),
 ]
